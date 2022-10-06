84d04fe7cfd13b5e49895113572ae4e96dbb2556
Stuart Todd <stuarttodd444@gmail.com>
2022-10-06 14:28:31 +0100

    server 500 debug

--- app/Http/Controllers/TurtlesController.php
+++ app/Http/Controllers/TurtlesController.php
@@ -16,7 +16,7 @@ class TurtlesController extends Controller
      */
     public function good(Request $request): View
     {
-        $leo = (new CreateLeonardoAction());
+        $leo = (new CreateLeonardoAction())->notify();
         dd($leo);
         return view('welcome', [
             'turtles' => [
