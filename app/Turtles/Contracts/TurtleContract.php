d819002843f1bdcf4571826707b1826c40fa330b
Stuart Todd <stuarttodd444@gmail.com>
2022-10-06 13:23:10 +0100

    populate turtleContract

--- app/Turtles/Contracts/TurtleContract.php
+++ app/Turtles/Contracts/TurtleContract.php
@@ -4,5 +4,18 @@ namespace App\Turtles\Contracts;
 
 interface TurtleContract
 {
+    /**
+     * When a turtle attacks
+     */
+    public function attack(): string;
 
+    /**
+     * When a turtle takes damage
+     */
+    public function damage(?Closure $damage = null): void;
+
+    /**
+     * @return array
+     */
+    public function toArray(): array;
 }
